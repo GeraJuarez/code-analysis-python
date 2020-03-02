9bc6bcd5acf95b60b932ed0e504c1254be0a5a74
GeraJuarez <geracjuarez@gmail.com>
2020-03-01 21:24:34 -0600

    create uni testing

--- lab3/UserDirectoryManager.py
+++ lab3/UserDirectoryManager.py
@@ -11,7 +11,7 @@ class UserDirectoryManager():
 
     def __init__(self):
         self.email_dir = {} # key: email, val: user obj
-        self.age_dir = {} # key: age, val: list of emails
+        self.age_dir = {}   # key: age, val: list of emails
 
     def __not_none(self, name, email, age, origin):
         return any( [val is None for val in [name, email, age, origin]] )
@@ -21,10 +21,10 @@ class UserDirectoryManager():
 
     def add_record(self, name, email, age, origin):
         if self.__not_none(name, email, age, origin):
-            raise Exception('All new user values must not be None')
+            raise TypeError('All new user values must not be None')
 
         if self.__repeated_email(email):
-            raise Exception('User email already registered')
+            raise TypeError('User email already registered')
         
         new_user = self.User(name, email, age, origin)
         self.email_dir[email] = new_user
@@ -32,15 +32,18 @@ class UserDirectoryManager():
         # Create list of users by age
         self.age_dir.setdefault(age, []).append(email)
 
-    def delete_record(self, email):
+        return self.email_dir[email]
+
+    def delete_record_by_email(self, email):
         user_deleted = self.email_dir.pop(email, None)
 
         if user_deleted:
             emails_by_age = self.age_dir.get(user_deleted.age)
             emails_by_age.remove(user_deleted.email)
+            if len(emails_by_age) == 0:
+                self.age_dir.pop(user_deleted.age)
 
         return user_deleted
-
 
     def search_by_email(self, email):
         return self.email_dir.get(email, None)
--- lab3/test_userManager.py
+++ lab3/test_userManager.py
@@ -3,52 +3,147 @@ from UserDirectoryManager import UserDirectoryManager as UDM
 
 class TestingFilecmp(unittest.TestCase):
     def test_add_record(self):
-        pass
+        udm = UDM()
+        output = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+
+        self.assertIsNotNone(output)
 
     def test_add_record_null(self):
-        pass
+        udm = UDM()
+        expected = TypeError
+
+        with self.assertRaises(expected):
+            udm.add_record(None, 'g@g.com', 22, 'Somewhere')
 
     def test_add_record_repeated_email(self):
-        pass
+        udm = UDM()
+        expected = TypeError
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+
+        with self.assertRaises(expected):
+            udm.add_record('name2', 'g@g.com', 21, 'over there')
 
     def test_del_record(self):
-        pass
+        udm = UDM()
+        output = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        deleted = udm.delete_record_by_email('g@g.com')
+        
+        self.assertEqual(output, deleted)
+        self.assertIsNone(udm.email_dir.get('g@g.com'))
+        self.assertIsNone(udm.age_dir.get(22))
 
     def test_del_record_none(self):
-        pass
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        deleted = udm.delete_record_by_email(None)
+
+        self.assertIsNone(deleted)
 
     def test_del_record_non_existent(self):
-        pass
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        deleted = udm.delete_record_by_email('g2@g.com')
+
+        self.assertIsNone(deleted)
     
     def test_search_email(self):
-        pass
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        found = udm.search_by_email('g@g.com')
+
+        self.assertIsNotNone(found)
+        self.assertEqual('name', found.name)
 
     def test_search_email_deleted(self):
-        pass
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        udm.delete_record_by_email('g@g.com')
+        found = udm.search_by_email('g@g.com')
+        
+        self.assertIsNone(found)
 
     def test_search_email_non_existent(self):
-        pass
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        udm.delete_record_by_email('g@g.com')
+        found = udm.search_by_email('g2@g.com')
+        
+        self.assertIsNone(found)
+
+    def test_search_email_other_type(self):
+        udm = UDM()
+        udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        found = udm.search_by_email(22)
+        
+        self.assertIsNone(found)
 
     def test_search_age(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        created2 = udm.add_record('name2', 'g2@g.com', 22, 'Somewhere')
+        found = udm.search_by_age(22)
+
+        self.assertTrue(found)
+        self.assertIn(created1, found)
+        self.assertIn(created2, found)
 
     def test_search_age_non_existent(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        created2 = udm.add_record('name2', 'g2@g.com', 22, 'Somewhere')
+        found = udm.search_by_age(21)
+
+        self.assertFalse(found)
+        self.assertNotIn(created1, found)
+        self.assertNotIn(created2, found)
 
     def test_search_age_none(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        found = udm.search_by_age(None)
+
+        self.assertFalse(found)
+        self.assertNotIn(created1, found)
 
     def test_search_age_deleted(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        created2 = udm.add_record('name2', 'g2@g.com', 22, 'Somewhere')
+        udm.delete_record_by_email('g2@g.com')
+        found = udm.search_by_age(22)
+
+        self.assertTrue(found)
+        self.assertIn(created1, found)
+        self.assertNotIn(created2, found)
 
     def test_get_all_empty(self):
-        pass
+        udm = UDM()
+        records = udm.get_all_records()
+
+        self.assertFalse(records)
     
     def test_get_all_filled(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        created2 = udm.add_record('name2', 'g2@g.com', 23, 'over there')
+        created3 = udm.add_record('name2', 'g3@g.com', 23, 'not Somewhere')
+        records = udm.get_all_records()
+
+        self.assertTrue(records)
+        self.assertEqual(3, len(records))
+        self.assertIn(created1, records)
 
     def test_get_all_after_deletion(self):
-        pass
+        udm = UDM()
+        created1 = udm.add_record('name', 'g@g.com', 22, 'Somewhere')
+        created2 = udm.add_record('name2', 'g2@g.com', 23, 'over there')
+        created3 = udm.add_record('name2', 'g3@g.com', 23, 'not Somewhere')
+        udm.delete_record_by_email('g@g.com')
+        records = udm.get_all_records()
+
+        self.assertTrue(records)
+        self.assertEqual(2, len(records))
+        self.assertNotIn(created1, records)
 
 if __name__ == "__main__":
     unittest.main()
