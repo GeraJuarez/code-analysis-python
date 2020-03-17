57c408670b1552cafe61d7b3e07071d78a82cedc
GeraJuarez <geracjuarez@gmail.com>
2020-03-16 21:55:59 -0600

    lab5

---
+++ lab5/DataSorter.py
@@ -0,0 +1,68 @@
+import csv
+
+
+class DataSorter(object):
+    def __init__(self):
+        self.data = []
+
+    def set_input_data(self, file_path_name):
+        if not file_path_name.endswith('.csv'):
+            raise IOError('File must be a csv file')
+
+        with open(file_path_name, 'r') as fp:
+            reader = csv.reader(fp, delimiter=',', skipinitialspace=True)
+            for row in reader:
+                self.data += row
+
+    def set_output_data(self, file_path_name):
+        with open(file_path_name, 'w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(self.data)
+
+    def execute_merge_sort(self, data=None):
+        if data is None:
+            data = self.data
+
+        self.__merge_sort(data, 0, len(data))
+
+    def __merge_sort(self, arr, idx_start, idx_end):
+        if len(arr[idx_start:idx_end]) < 2:
+            return
+
+        if idx_start < idx_end:
+            idx_mid = (idx_start + idx_end) // 2
+            self.__merge_sort(arr, idx_start, idx_mid)
+            self.__merge_sort(arr, idx_mid, idx_end)
+            self.__merge(arr, idx_start, idx_mid, idx_end)
+
+    def __merge(self, arr, idx_start, idx_mid, idx_end):
+        temp = []
+        iter_left = idx_start
+        iter_right = idx_mid
+
+        for _ in range(idx_end - idx_start):
+            if iter_left == idx_mid:
+                temp.append(arr[iter_right])
+                iter_right += 1
+
+            elif iter_right == idx_end:
+                temp.append(arr[iter_left])
+                iter_left += 1
+
+            elif arr[iter_left] <= arr[iter_right]:
+                temp.append(arr[iter_left])
+                iter_left += 1
+
+            else:
+                temp.append(arr[iter_right])
+                iter_right += 1
+
+        for idx, val in enumerate(temp):
+            arr[idx_start + idx] = val
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sorter = DataSorter()
+    sorter.set_input_data('lab5/sample.csv')
+    sorter.execute_merge_sort()
+    sorter.set_output_data('lab5/s.csv')
---
+++ lab5/test_dataSorter.py
@@ -0,0 +1,53 @@
+import unittest
+import csv
+from DataSorter import DataSorter
+
+
+class TestDataSorter(unittest.TestCase):
+    def is_sorted(self, arr):
+        return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
+
+    def test_merge_sort1(self):
+        ds = DataSorter()
+        arr = [5, 2, 1, 3, 5]
+        ds.execute_merge_sort(data=arr)
+        self.assertTrue(self.is_sorted(arr))
+
+    def test_merge_sort2(self):
+        ds = DataSorter()
+        arr = [5, 3, 1]
+        ds.execute_merge_sort(data=arr)
+        self.assertTrue(self.is_sorted(arr))
+
+    def test_merge_sort3(self):
+        ds = DataSorter()
+        arr = ['z2', 'a', 'b', '123', '512']
+        ds.execute_merge_sort(data=arr)
+        self.assertTrue(self.is_sorted(arr))
+
+    def test_load_file(self):
+        ds = DataSorter()
+        ds.set_input_data('sample.csv')
+        ds.execute_merge_sort()
+        self.assertTrue(self.is_sorted(ds.data))
+
+    def test_load_file_err1(self):
+        ds = DataSorter()
+        with self.assertRaises(IOError):
+            ds.set_input_data('sample.txt')
+
+    def test_save_file(self):
+        ds = DataSorter()
+        ds.set_input_data('sample.csv')
+        ds.execute_merge_sort()
+        ds.set_output_data('out.csv')
+
+        with open('out.csv', 'r') as fp:
+            reader = csv.reader(fp, delimiter=',', skipinitialspace=True)
+            data = next(reader)
+
+        self.assertEqual(data, ds.data)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
