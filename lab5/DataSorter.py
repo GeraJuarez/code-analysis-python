9dd83fc8632e53b29fe206d437e8660d753aaae7
GeraJuarez <geracjuarez@gmail.com>
2020-03-22 23:11:15 -0600

    docuemntation for lab5 files

--- lab5/DataSorter.py
+++ lab5/DataSorter.py
@@ -2,10 +2,27 @@ import csv
 
 
 class DataSorter(object):
+    """The DataSorter class save csv files and sortes them.
+
+    It can read a csv file and sort it at increasing order with
+    the merge sort algorithm, the result can then be saved in another file.
+    """
+
     def __init__(self):
+        """The constructor. Initialize a list."""
+
         self.data = []
 
     def set_input_data(self, file_path_name):
+        """Read the csv file in the specified path
+
+        Args:
+            file_path_name: The path of the csv file
+
+        Raises:
+            TypeError, IOError
+        """
+
         if file_path_name is None:
             raise TypeError('File path must not be None')
 
@@ -18,17 +35,54 @@ class DataSorter(object):
                 self.data += row
 
     def set_output_data(self, file_path_name):
+        """Write a csv file in the specified path with the saved data
+
+        Args:
+            file_path_name: The path of the csv file to be written
+
+        Raises:
+            IOError
+        """
+
         with open(file_path_name, 'w') as fp:
             writer = csv.writer(fp)
             writer.writerow(self.data)
 
     def execute_merge_sort(self, data=None):
+        """Apply the mergesort algorithm to the given list
+
+        When data is None, it will sort the data/list saved in the
+        instantiated class.
+
+        Args:
+            data: The list to be sorted. When None, it will use the
+            list saved in this class.
+
+        Raises:
+            TypeError
+        """
+
         if data is None:
             data = self.data
+
+        if type(data) is not list:
+            raise TypeError('given data must be a list')
 
         self.__merge_sort(data, 0, len(data))
 
     def __merge_sort(self, arr, idx_start, idx_end):
+        """The actual mergesort algorithm
+
+        It splits the list by half until the base case arrives, that is
+        when a list of 2 or three elements is reached. Then, it continues
+        to the merge section.
+
+        Args:
+            arr: The list to be sorted
+            idx_start: the first index of the array/list
+            idx_end: the last index of the array/list
+        """
+
         if len(arr[idx_start:idx_end]) < 2:
             return
 
@@ -39,6 +93,17 @@ class DataSorter(object):
             self.__merge(arr, idx_start, idx_mid, idx_end)
 
     def __merge(self, arr, idx_start, idx_mid, idx_end):
+        """The merge step of the mergesort algorithm
+
+        It merges two sorted arrays into a single one.
+
+        Args:
+            arr: The list to be sorted
+            idx_start: the first index of the array/list
+            idx_mid: the middle index of the array/list
+            idx_end: the last index of the array/list
+        """
+
         temp = []
         iter_left = idx_start
         iter_right = idx_mid
--- lab5/test_dataSorter.py
+++ lab5/test_dataSorter.py
@@ -4,50 +4,88 @@ from DataSorter import DataSorter
 
 
 class TestDataSorter(unittest.TestCase):
+    """The TestDataSorter class runs some unit tests for the DataSorter class.
+    """
+
     def is_sorted(self, arr):
+        """Validates for a sorted array/list.
+
+        Args:
+            arr: the list to be validated.
+
+        Returns:
+            True if the given array is sorted.
+        """
+
         return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
 
     def test_merge_sort1(self):
+        """Tests the mergesort algorithm with an list of integers.
+        """
+
         ds = DataSorter()
         arr = [5, 2, 1, 3, 5]
         ds.execute_merge_sort(data=arr)
         self.assertTrue(self.is_sorted(arr))
 
     def test_merge_sort2(self):
+        """Tests if the mergesort algorithm with an list of
+        integers with an odd length.
+        """
+
         ds = DataSorter()
         arr = [5, 3, 1]
         ds.execute_merge_sort(data=arr)
         self.assertTrue(self.is_sorted(arr))
 
     def test_merge_sort3(self):
+        """Test that validates the use of the DataSorter list when
+        a None value is given to the mergesort method of the class.
+        """
+
         ds = DataSorter()
         arr = None
         ds.execute_merge_sort(data=arr)
         self.assertFalse(ds.data)
 
     def test_merge_sort4(self):
+        """Test the correctness of the mergesort when given a list of strings.
+        """
+
         ds = DataSorter()
         arr = ['z2', 'a', 'b', '123', '512']
         ds.execute_merge_sort(data=arr)
         self.assertTrue(self.is_sorted(arr))
 
     def test_load_file(self):
+        """Test that a file is actually read and sorted by the class.
+        """
+
         ds = DataSorter()
         ds.set_input_data('sample.csv')
         ds.execute_merge_sort()
         self.assertTrue(self.is_sorted(ds.data))
 
     def test_load_file_err1(self):
+        """Test that IOError is raised with a non csv-file.
+        """
+
         ds = DataSorter()
         with self.assertRaises(IOError):
             ds.set_input_data('sample.txt')
 
     def test_load_file_err2(self):
+        """Test that TypeError is raised with an invalid path.
+        """
+
         ds = DataSorter()
         with self.assertRaises(TypeError):
             ds.set_input_data(None)
 
     def test_save_file(self):
+        """Test that the saved csv-file is sorted.
+        """
+
         ds = DataSorter()
         ds.set_input_data('sample.csv')
         ds.execute_merge_sort()
