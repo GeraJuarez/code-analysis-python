058a63f457ae717927a6273837e5fded7aa5221c
GeraJuarez <geracjuarez@gmail.com>
2020-03-23 00:03:25 -0600

    finish documenting lab 3

--- lab3/test_factorial.py
+++ lab3/test_factorial.py
@@ -3,7 +3,14 @@ import math
 
 
 class TestingFactorial(unittest.TestCase):
+    """The TestingFactorial class runs some unit tests
+    for the math.factorial function.
+    """
+
     def test_one(self):
+        """Test the base case of 1.
+        """
+
         input_ = 1
         output = math.factorial(input_)
         expected = 1
@@ -12,6 +19,9 @@ class TestingFactorial(unittest.TestCase):
                          f'Result: {output}, expectd: {expected}')
 
     def test_zero(self):
+        """Test the base case of 0.
+        """
+
         input_ = 0
         output = math.factorial(input_)
         expected = 1
@@ -20,6 +30,9 @@ class TestingFactorial(unittest.TestCase):
                          f'Result: {output}, expectd: {expected}')
 
     def test_10(self):
+        """Test for input of 10.
+        """
+
         input_ = 10
         output = math.factorial(input_)
         expected = 3628800
@@ -28,36 +41,54 @@ class TestingFactorial(unittest.TestCase):
                          f'Result: {output}, expectd: {expected}')
 
     def test_negative(self):
+        """Test for negative number ValueError.
+        """
+
         input_ = -1
         expected = ValueError
         with self.assertRaises(expected):
             math.factorial(input_)
 
     def test_float_type(self):
+        """Test for invalid type float. ValueError.
+        """
+
         input_ = 1.2
         expected = ValueError
         with self.assertRaises(expected):
             math.factorial(input_)
 
     def test_str_type(self):
+        """Test for invalid type str. ValueError.
+        """
+
         expected = TypeError
         input_ = 'c'
         with self.assertRaises(expected):
             math.factorial(input_)
 
     def test_list_type(self):
+        """Test for invalid type list. ValueError.
+        """
+
         expected = TypeError
         input_ = []
         with self.assertRaises(expected):
             math.factorial(input_)
 
     def test_dict_type(self):
+        """Test for invalid type dict. ValueError.
+        """
+
         expected = TypeError
         input_ = []
         with self.assertRaises(expected):
             math.factorial(input_)
 
     def test_none_type(self):
+        """Test for invalid type None. ValueError.
+        """
+
         expected = TypeError
         input_ = None
         with self.assertRaises(expected):
--- lab3/test_filecmp.py
+++ lab3/test_filecmp.py
@@ -3,7 +3,14 @@ import filecmp
 
 
 class TestingFilecmp(unittest.TestCase):
+    """The TestingFactorial class runs some unit tests
+    for the filecmp.cmp function.
+    """
+
     def test_same_file(self):
+        """Test the comparison of a file with itself.
+        """
+
         input1 = '1.txt'
         input2 = '1.txt'
         output = filecmp.cmp(input1, input2)
@@ -11,6 +18,9 @@ class TestingFilecmp(unittest.TestCase):
         self.assertTrue(output)
 
     def test_same_extension_same_content(self):
+        """Test for True comparing equal files with different name.
+        """
+
         input1 = '1.txt'
         input2 = '2.txt'
         output = filecmp.cmp(input1, input2)
@@ -18,6 +28,9 @@ class TestingFilecmp(unittest.TestCase):
         self.assertTrue(output)
 
     def test_diff_extension_same_content(self):
+        """Test for True comparing equal files with different extension.
+        """
+
         input1 = '2.txt'
         input2 = '2.csv'
         output = filecmp.cmp(input1, input2)
@@ -25,6 +38,9 @@ class TestingFilecmp(unittest.TestCase):
         self.assertTrue(output)
 
     def test_same_extension_diff_content(self):
+        """Test for False comparing different files with same extension.
+        """
+
         input1 = '1.txt'
         input2 = '3.txt'
         output = filecmp.cmp(input1, input2)
@@ -32,6 +48,9 @@ class TestingFilecmp(unittest.TestCase):
         self.assertFalse(output)
 
     def test_diff_extension_diff_content(self):
+        """Test for False comparing different files with different extension.
+        """
+
         input1 = '2.csv'
         input2 = '3.txt'
         output = filecmp.cmp(input1, input2)
